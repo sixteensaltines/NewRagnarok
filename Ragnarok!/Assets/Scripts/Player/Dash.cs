1f701972f27057b2bbde304ba53389a17afc9361
Sixteensaltines <facuaguerog@gmail.com>
2020-02-25 19:58:57 -0300

    Script Del Dash Sin Terminar
    No puedo hacer que dashee hacia atras estando quieto

---
+++ Ragnarok!/Assets/Scripts/Player/Dash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+
+    public Inputs En_Inputs;
+
+    public float FuerzaDash;
+    public bool SeEstaMoviendo = false;
+    Rigidbody2D R_Player;
+
+    void Start()
+    {
+        R_Player = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (En_Inputs.B_Dash)
+        {
+            SeEstaMoviendo = true;
+            R_Player.velocity = Vector2.right * FuerzaDash;
+            En_Inputs.B_Dash = false;         
+            Invoke("CheckMov", 0.75f);
+
+        }
+
+    }
+    public void CheckMov()
+    {
+        SeEstaMoviendo = false;
+        R_Player.velocity = Vector2.zero;
+    }
+}
