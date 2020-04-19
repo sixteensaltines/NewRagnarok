397269e43fc61630994e904809c54c4e48a8abbf
Sixteensaltines <facuaguerog@gmail.com>
2020-04-19 00:49:46 -0300

    Caminata Base Sin corregir
    Una caminata bastante chota, hace falta algunos cambios para que sea mas notorio el movimiento en  el torso y cambiar el spanglish

---
+++ CopiaRepositorioSecundario/Facu/Unitys/Player Mov/Assets/Scripts/Player/AnimacionPiernas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionPiernas : MonoBehaviour
+{
+    public Inputs En_Inputs;
+    public Animator Anim;
+
+    public void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+    public void Update()
+    {
+        if (En_Inputs.BH_Right || En_Inputs.BH_Left)
+        {
+            Anim.SetBool("WalkLegs", true);
+        }
+        else
+        {
+            Anim.SetBool("WalkLegs", false);
+        }
+    }
+}
