146693fe5b9ac73d2cce79b7088d0929f662417a
Sixteensaltines <facuaguerog@gmail.com>
2020-05-23 16:16:55 -0300

    Animaciones Nueva Formula
    Encontre la forma definitiva de hacer animaciones, despues de muchos cambios encontre la forma mas optima que pude conseguir para hacer animaciones de la version final. Uso Layers en el animator, divido entre body, legs y torso las animaciones

---
+++ CopiaRepositorioSecundario/Facu/Unitys/Player Mov/Assets/PruebaAnimacion/AnimacionDoble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionDoble : MonoBehaviour
+{
+
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            anim.SetBool("Arriba", true);
+        }
+        else
+        {
+            anim.SetBool("Arriba", false);
+        }
+        if (Input.GetKey(KeyCode.N))
+        {
+            anim.SetBool("Abajo", true);
+        }
+        else
+        {
+            anim.SetBool("Abajo", false);
+        }
+
+    }
+}
