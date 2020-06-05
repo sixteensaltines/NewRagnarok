dd032f2072d09117e00a12eeee21d61393e60ea2
Sixteensaltines <facuaguerog@gmail.com>
2020-06-04 22:46:47 -0300

    Nuevo Punto De control animacion y Parallax
    Por las dudas guardo puntos de control para la animacion, y se implemento el parallax sencillo en la nueva escena. En unas horas se agregan los nuevos modelos de parallax.

---
+++ CopiaRepositorioSecundario/Facu/Unitys/EscenaFinalRagnarok/Assets/Scripts/BackGround/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    
+    public GameObject Camara;
+    public float EfectoParallax; 
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+    void FixedUpdate()
+    {
+        float temp = (Camara.transform.position.x * (1 - EfectoParallax));
+        float dist = (Camara.transform.position.x * EfectoParallax);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else 
+        if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
